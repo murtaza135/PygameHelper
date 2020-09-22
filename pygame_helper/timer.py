bb4fe1c486bb3426b8cdccef0977236cf001121d
murtaza akhtar <42249079+murtaza135@users.noreply.github.com>
2020-09-22 23:07:19 +0100

    cleaned up code and architecture in Timer class

--- pygame_helper/timer.py
+++ pygame_helper/timer.py
@@ -4,67 +4,74 @@ import pygame_helper.exceptions as exceptions
 
 class Timer(object):
     
-    def __init__(self, countdown):
+    def __init__(self, countdown, auto_start=False, set_timer_to_completed_when_reset=True):
         if not pygame.display.get_init():
             raise exceptions.PygameInitError
         
         self._time_started_seconds = None
         self._countdown = None
-        self.set_new_delay(countdown)
+        self.countdown = countdown
+        self._is_timer_completed_when_reset = set_timer_to_completed_when_reset
+        self._is_reset_called = True
 
-    def set_new_delay(self, countdown):
-        if countdown <= 0:
-            raise ValueError("Must provide a valid delay time (in seconds)")
+        if auto_start:
+            self._start_new_timer()
 
-        self._countdown = float(countdown)
 
     def _start_new_timer(self):
-        self._time_started_seconds = self.get_current_time()
+        self._time_started_seconds = self._current_time
+        self._is_reset_called = False
 
     def start(self):
-        if self.is_completed():
+        if self._time_started_seconds is None or self.is_completed():
             self._start_new_timer()
+
+    def restart(self):
+        self._start_new_timer()
 
     def reset(self):
-        if self.is_completed():
-            self._start_new_timer()
+        self._time_started_seconds = None
+        self._is_reset_called = True
 
-    def force_reset(self):
-        self._start_new_timer()
 
     def is_running(self):
         return not self.is_completed()
 
     def is_completed(self):
-        return (
-            self._time_started_seconds is None
-            or self._time_started_seconds + self._countdown <= self.get_current_time()
-        )
+        if self._is_reset_called:
+            return self._is_timer_completed_when_reset
+        else:
+            return self._time_started_seconds + self._countdown <= self._current_time
 
-    def get_current_time(self):
-        return float(pygame.time.get_ticks() / 1000)
 
-    def get_time_remaining(self):
+    @property
+    def _current_time(self):
+        return float(pygame.time.get_ticks()) / 1000
+
+    @property
+    def time_remaining(self):
         if self._time_started_seconds is None:
             return float(0)
 
-        time_remaining_seconds = (self._time_started_seconds + self._countdown) - self.get_current_time()
+        time_remaining_seconds = (self._time_started_seconds + self._countdown) - self._current_time
         if time_remaining_seconds < 0:
             time_remaining_seconds = 0
         return time_remaining_seconds
 
+    @property
+    def countdown(self):
+        return self._countdown
+
+    @countdown.setter
+    def countdown(self, countdown):
+        if countdown < 0:
+            raise ValueError("Must provide a valid countdown time (in seconds)")
+        self._countdown = float(countdown)
+
+
     def __repr__(self):
-        return f"Timer(countdown={self._countdown})"
+        return f"Timer(countdown={self._countdown}, set_timer_to_completed_when_reset={self._is_timer_completed_when_reset})"
 
     def __str__(self):
-        time_remaining = round(self.get_time_remaining(), 5)
-        return f"Time remaining: {time_remaining} seconds"
-
-    def __setattr__(self, name, time_seconds):
-        if time_seconds is not None and time_seconds < 0:
-            raise ValueError("Must provide a valid delay time (in seconds)")
-
-        super().__setattr__(name, time_seconds)
-
-    def __delattr__(self, name):
-        raise exceptions.DeleteError("Cannot delete attribute")
+        time_remaining_seconds = round(self.time_remaining, 5)
+        return f"Time remaining: {time_remaining_seconds} seconds"
